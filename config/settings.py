3f8e96eb550704f2ba7709f2c5f32036aaea4621
crishdz690@gmail.com <Crishdz690@gmail.com>
2026-06-02 17:02:07 -0600

    feat: Integración de servidor SMTP y plantillas de correo
    - Se configuró el envío de correos reales mediante Gmail SMTP.
    
    - Se aseguraron las credenciales utilizando variables de entorno (.env).
    
    - Se personalizaron las plantillas de asunto y cuerpo del correo para la recuperación de contraseñas.

--- config/settings.py
+++ config/settings.py
@@ -111,4 +111,10 @@ LOGOUT_REDIRECT_URL = 'lista_noticias'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Configuración de Correo Real (SMTP Gmail)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
