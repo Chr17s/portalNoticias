0117c7f424d4650c743dc41a026e5ef2621fa2d7
crishdz690@gmail.com <Crishdz690@gmail.com>
2026-06-02 22:52:48 -0600

    fix: Corrección de ruta absoluta en STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Tu configuración actual (Para tus archivos locales de desarrollo)
 STATICFILES_DIRS = [BASE_DIR / 'static'] if os.path.exists(BASE_DIR / 'static') else []
